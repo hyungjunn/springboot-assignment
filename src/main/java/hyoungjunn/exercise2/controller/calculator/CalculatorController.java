5336ef537550175ee10cee0648592f407147f065
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-21 09:40:49 +0900

    refactor addTwoNumber method parameter

--- src/main/java/hyoungjunn/exercise2/controller/calculator/CalculatorController.java
+++ src/main/java/hyoungjunn/exercise2/controller/calculator/CalculatorController.java
@@ -1,5 +1,6 @@
 package hyoungjunn.exercise2.controller.calculator;
 
+import hyoungjunn.exercise2.dto.calculator.request.CalculatorAddRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculatorController {
 
     @GetMapping("/add")
-    public int addTwoNumber(@RequestParam int number1, @RequestParam int number2) {
-        return number1 + number2;
+    public int addTwoNumber(CalculatorAddRequest request) {
+        return request.getNumber1() + request.getNumber2();
     }
 }
