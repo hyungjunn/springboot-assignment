d326985ece39a06274df9ceb3efe0c2dafe6c62f
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-03 11:50:04 +0900

    style remove comment

--- src/main/java/hyoungjunn/exercise2/controller/UserController.java
+++ src/main/java/hyoungjunn/exercise2/controller/UserController.java
@@ -1,24 +1,18 @@
 package hyoungjunn.exercise2.controller;
 
-import hyoungjunn.exercise2.domain.User;
 import hyoungjunn.exercise2.dto.request.UserSaveRequest;
 import hyoungjunn.exercise2.dto.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    //private final List<User> users = new ArrayList<>();
     private final JdbcTemplate jdbcTemplate;
 
     public UserController(JdbcTemplate jdbcTemplate) {
@@ -27,17 +21,12 @@ public class UserController {
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserSaveRequest request) {
-        //users.add(new User(request.getName(), request.getAge()));
         String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
         jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUser() {
-//        List<UserResponse> responses = new ArrayList<>();
-//        for (int i = 0; i < users.size(); i++) {
-//            responses.add(new UserResponse(i + 1, users.get(i)));
-//        }
         String sql = "SELECT * FROM user";
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
             long id = rs.getLong("id");
