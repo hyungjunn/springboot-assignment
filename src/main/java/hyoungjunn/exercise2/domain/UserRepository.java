4e071fffa26cb3fe8244921c30fedd40ee11f1be
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-04 09:39:30 +0900

    userfindbyid return type change user to optional<user>

--- src/main/java/hyoungjunn/exercise2/domain/UserRepository.java
+++ src/main/java/hyoungjunn/exercise2/domain/UserRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByName(String name);
+    // find라고 입력하면 1개의 데이터만 가져오기 때문에 반환타입을 User로 한다.
+    // 더 깔끔하게 하기 위해 Optional을 쓴다
+    Optional<User> findByName(String name);
 
 }
